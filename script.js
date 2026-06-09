6941e80492ca3cd21c99da68d19e4cdddc5ca0a3
nicole926 <nicolemendes.dev@gmail.com>
2026-06-09 16:24:05 -0300

    commit

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ const menu = document.getElementById('menu');
 menuIcon.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
+
+//tema
+const btnTema = document.getElementById('theme');
+const body = document.body;
+const temaSalvo = localStorage.getItem('tema');
+
+temaEscuro(temaSalvo === 'darkmode');
+
+function temaEscuro(tipo){
+    if(tipo){
+        body.classList.add('darkmode');
+        btnTema.innerHTML = '<i class="fa-solid fa-sun"></i> Theme';
+    } else {
+        body.classList.remove('darkmode');
+        btnTema.innerHTML = '<i class="fa-solid fa-moon"></i> Theme';
+    }
+}
+
+btnTema.addEventListener('click', () => {
+    const isDark = !body.classList.contains('darkmode');
+    temaEscuro(isDark);
+    localStorage.setItem(
+        'tema', isDark ? 'darkmode' : 'claro'
+    );
+});
