d94173dc92b61b3d578142f933106b8713fe90d7
nicole926 <nicolemendes.dev@gmail.com>
2026-06-11 16:11:23 -0300

    commit

--- script.js
+++ script.js
@@ -30,3 +30,47 @@ btnTema.addEventListener('click', () => {
         'tema', isDark ? 'darkmode' : 'claro'
     );
 });
+
+// sistema
+
+// clima atual
+const apiKey = "7a3830947cea0cd067c065505ee93f7a";
+const cidade = "Oeiras";
+
+// clima principal: cidade, humidade, temperatura e icon
+
+// cidade
+const cidadeElemento = document.getElementById('cidade');
+const umidadeElemento = document.getElementById('umidade');
+const temperaturaElemento = document.getElementById('temperatura');
+const imgIcon = document.getElementById('imagemTemp');
+
+fetch(
+  `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br`
+)
+.then(response => response.json())
+.then(data => {
+    cidadeElemento.textContent = data.name;
+    umidadeElemento.textContent = data.main.humidity;
+    temperaturaElemento.textContent = `${Math.round(data.main.temp)}°C`;
+    const codigo = data.weather[0].icon;
+
+    const icones = {
+        "01d": "img/appclima-manhãensolarada.png",
+        "01n": "img/appclima-noite.png",
+        "02d": "img/appclima-manhãnublada.png",
+        "02n": "img/appclima-noite.png",
+        "03d": "img/cloudy.png",
+        "04d": "img/cloudy.png",
+        "09d": "img/rain.png",
+        "10d": "img/rain.png",
+        "11d": "img/storm.png"
+    };
+
+imgIcon.src = icones[codigo];
+});
+
+// gráfico de temperatura
+fetch(
+  `https://api.openweathermap.org/data/2.5/forecast?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br`
+)
